e0d62b4883b1b8024ef573bd6a310117dace0768
Kun Yan <kyan@redhat.com>
2017-01-26 10:16:05 +0800

    Add babel support for es6

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const Koa = require('koa');
 const app = new Koa();
 
 // response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 app.listen(8080);
